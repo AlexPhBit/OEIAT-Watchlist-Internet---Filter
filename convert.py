b6f43b482c7b30a7a4d20652798b5ff411034ac6
AlexPhBit <62428017+AlexPhBit@users.noreply.github.com>
2026-02-16 21:34:54 +0100

    Modify output format for filter.txt generation

--- convert.py
+++ convert.py
@@ -2,8 +2,8 @@ import csv
 from datetime import datetime
 
 with open('data.csv', 'r', encoding='utf-8') as infile, open('filter.txt', 'w', encoding='utf-8') as outfile:
-    outfile.write('! ÖIAT Watchlist Internet - AdGuard Filter from CSV (https://www.watchlist-internet.at/liste-betruegerischer-shops/csv/)\n')
-    outfile.write(f'! Generated: {datetime.utcnow().isoformat()}Z\n')
+    # outfile.write('! ÖIAT Watchlist Internet - AdGuard Filter from CSV (https://www.watchlist-internet.at/liste-betruegerischer-shops/csv/)\n')
+    # outfile.write(f'! Generated: {datetime.utcnow().isoformat()}Z\n')
     
     reader = csv.reader(infile, delimiter=';')
     next(reader, None)  # Skip Header
@@ -12,4 +12,4 @@ with open('data.csv', 'r', encoding='utf-8') as infile, open('filter.txt', 'w', 
         if len(row) >= 1:
             domain = row[0].strip().strip('"')
             if domain and '.' in domain:
-                outfile.write(f'{domain}\n')
+                outfile.write(f'||{domain}^|\n')
